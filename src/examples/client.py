0abe74b29df5ac70cd5c28e2c44457d2e7b1b4b8
Brandon Wiley <brandon@blanu.net>
2012-06-29 13:25:09 -0500

    Reformatted code with PythonTidy

--- src/examples/client.py
+++ src/examples/client.py
@@ -1,4 +1,5 @@
-#!/usr/bin/env python -u
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
 
 import os
 import sys
@@ -11,50 +12,51 @@ from socket import inet_ntoa
 from pyptlib.config import EnvException
 from pyptlib.client import ClientConfig
 
+
 class UnsupportedManagedTransportVersionException(Exception):
-  pass
+
+    pass
+
 
 class NoSupportedTransportsException(Exception):
-  pass
+
+    pass
+
 
 class TransportLaunchException(Exception):
-  def __init__(self, message):
-    message=message
+
+    def __init__(self, message):
+        message = message
+
 
 def launchClient(self, name, port):
-  if name!=supportedTransport:
-    raise TransportLaunchException('Tried to launch unsupported transport %s' % (name))
+    if name != supportedTransport:
+        raise TransportLaunchException('Tried to launch unsupported transport %s'
+                 % name)
 
-if __name__=='__main__':
-    supportedTransportVersion='1'
-    supportedTransport='dummy'
-    config=ClientConfig()
 
-  if config.checkManagedTransportVersion(supportedTransportVersion):
-    config.writeVersion(supportedTransportVersion)
-  else:
-    config.writeVersionError()
-    raise UnsupportedManagedTransportVersionException()
+if __name__ == '__main__':
+    supportedTransportVersion = '1'
+    supportedTransport = 'dummy'
+    config = ClientConfig()
 
-  if not config.checkTransportEnabled(supportedTransport):
-    raise NoSupportedTransportsException()
-  except EnvException:
-    print('error 0')
-    return
-  except UnsupportedManagedTransportVersionException:
-    print('error 1')
-    return
-  except NoSupportedTransportsException:
-    print('error 2')
-    return
+    if config.checkManagedTransportVersion(supportedTransportVersion):
+        config.writeVersion(supportedTransportVersion)
+    else:
+        config.writeVersionError()
+        raise UnsupportedManagedTransportVersionException()
 
-  try:
-    launchClient(supportedTransport, 8182)
-    config.writeMethod(supportedTransport, 5, ('127.0.0.1', 8182), None, None)
-  except TransportLaunchException as e:
-    print('error 3')
-    config.writeMethodError(supportedTransport, e.message)
+    if not config.checkTransportEnabled(supportedTransport):
+        raise NoSupportedTransportsException()
 
-  config.writeMethodEnd()
+    try:
+        launchClient(supportedTransport, 8182)
+        config.writeMethod(supportedTransport, 5, ('127.0.0.1', 8182),
+                           None, None)
+    except TransportLaunchException, e:
+        print 'error 3'
+        config.writeMethodError(supportedTransport, e.message)
 
-  run()
+    config.writeMethodEnd()
+
+    run()
--- src/examples/server.py
+++ src/examples/server.py
@@ -1,4 +1,5 @@
-#!/usr/bin/env python -u
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
 
 import os
 import sys
@@ -9,36 +10,40 @@ from socket import inet_ntoa
 from pyptlib.config import EnvException
 from pyptlib.server import ServerConfig, MethodOptions
 
-supportedTransport='dummy'
+supportedTransport = 'dummy'
+
 
 def launchServer(self, name, port):
-  if name!=supportedTransport:
-    raise TransportLaunchException('Tried to launch unsupported transport %s' % (name))
+    if name != supportedTransport:
+        raise TransportLaunchException('Tried to launch unsupported transport %s'
+                 % name)
 
-if __name__=='__main__':
-  supportedTransportVersion='1'
-  config=ClientConfig()
-  try:
-    if config.checkManagedTransportVersion(supportedTransportVersion):
-      config.writeVersion(supportedTransportVersion)
-    else:
-      config.writeVersionError()
-      raise UnsupportedManagedTransportVersionException()
-  except EnvException:
-    print('error 0')
-    return
-  except UnsupportedManagedTransportVersionException:
-    print('error 1')
-    return
-  except NoSupportedTransportsException:
-    print('error 2')
-    return
 
-  try:
-    launchServer(supportedTransport, 8182)
-    config.writeMethod(supportedTransport, ('127.0.0.1', 8182), MethodOptions())
-  except TransportLaunchException as e:
-    print('error 3')
-    config.writeMethodError(supportedTransport, e.message)
+if __name__ == '__main__':
+    supportedTransportVersion = '1'
+    config = ClientConfig()
+    try:
+        if config.checkManagedTransportVersion(supportedTransportVersion):
+            config.writeVersion(supportedTransportVersion)
+        else:
+            config.writeVersionError()
+            raise UnsupportedManagedTransportVersionException()
+    except EnvException:
+        print 'error 0'
+        return
+    except UnsupportedManagedTransportVersionException:
+        print 'error 1'
+        return
+    except NoSupportedTransportsException:
+        print 'error 2'
+        return
 
-  config.writeMethodEnd()
+    try:
+        launchServer(supportedTransport, 8182)
+        config.writeMethod(supportedTransport, ('127.0.0.1', 8182),
+                           MethodOptions())
+    except TransportLaunchException, e:
+        print 'error 3'
+        config.writeMethodError(supportedTransport, e.message)
+
+    config.writeMethodEnd()
